50625e38b320826c0ac43574dc24f0668046bd85
Cannonball <rearick.seth@gmail.com>
2024-03-06 18:15:05 -0500

    First version of the month class with enum type

---
+++ Labs/11_Lab/Month.java
@@ -0,0 +1,58 @@
+class Month {
+
+    enum allMonths {
+                 January, Feburary, March, April,
+                 May, June, July, August,
+                 September, October, November, December
+                };
+
+    allMonths month;
+
+    public Month() {
+        this.setMonth(1);
+    }
+
+    public Month(int month) {
+        this.setMonth(month);
+    }
+
+    // Fill this out
+    public Month(String monthName) {
+        this.month = allMonths.valueOf(monthName);
+    }
+
+    public int getMonthNumber() {
+        return this.month.ordinal() + 1;
+    }    
+
+    public void setMonth(int monthNumber) {
+        this.month = allMonths.values()[monthNumber + 1];
+    }
+
+    public boolean equals(Month otherMonth) {
+        boolean result = (this.getMonthNumber() == otherMonth.getMonthNumber()) ? true : false;
+
+        return result;
+
+    }
+
+    public boolean greaterThan(Month otherMonth) {
+        boolean result = (this.getMonthNumber() > otherMonth.getMonthNumber()) ? true : false;
+
+        return result;
+    }
+
+    public boolean lessThan(Month otherMonth) {
+        boolean result = (this.getMonthNumber() < otherMonth.getMonthNumber()) ? true : false;
+
+        return result;
+    }
+
+    public String toString() {
+        String monthString = "Month Name: " + this.getMonthNumber() + "\n";
+
+        return monthString;
+    }
+
+    
+}
