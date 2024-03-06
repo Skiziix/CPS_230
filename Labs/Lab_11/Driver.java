43e1b2daa41a03955f08dae5169e4079e10a19ba
Cannonball <rearick.seth@gmail.com>
2024-03-06 18:46:50 -0500

    Changed Directory Name

--- Labs/Lab_11/Driver.java
+++
@@ -1,9 +0,0 @@
-package CPS_230.Labs.Lab_11;
-
-public class Driver {
-    public static void main(String[] args) {
-        Month month = new Month("January");
-
-        System.out.println("\n\n" + month);
-    }
-}
