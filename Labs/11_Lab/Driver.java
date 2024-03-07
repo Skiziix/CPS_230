288c24243e58a8733f3da5166f32b9c396fba259
Cannonball <rearick.seth@gmail.com>
2024-03-06 19:12:35 -0500

    Driver catches an InvalidMonthName

--- Labs/11_Lab/Driver.java
+++ Labs/11_Lab/Driver.java
@@ -1,7 +1,15 @@
 public class Driver {
     public static void main(String[] args) {
-        Month month = new Month("January");
+        Month month;
+        try {
+            month = new Month("January");
+            System.out.println("\n\n" + month);
 
-        System.out.println("\n\n" + month);
+        } catch (InvalidMonthName e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
     }
 }
