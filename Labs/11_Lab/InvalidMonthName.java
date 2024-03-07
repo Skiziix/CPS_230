db1ca8aa64bd2345ec9c9039ec7d996a05d8145c
Cannonball <rearick.seth@gmail.com>
2024-03-06 21:01:56 -0500

    Added comments

--- Labs/11_Lab/InvalidMonthName.java
+++ Labs/11_Lab/InvalidMonthName.java
@@ -1,9 +1,15 @@
+/*
+ * Exceptions for the Month class in the event of an invalid month name
+ */
+
 public class InvalidMonthName extends Exception{
     
+    // Default message
     public InvalidMonthName() {
         super("Please retype the month. That was invalid!");
     }
 
+    // User context message
     public InvalidMonthName(String monthName) {
         super(monthName + " is an invalid month. Please retype!");
     }
