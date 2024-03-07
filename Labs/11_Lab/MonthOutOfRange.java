4d35ef9c1215d9149a655de07edf90446fb8ce25
Cannonball <rearick.seth@gmail.com>
2024-03-06 21:02:59 -0500

    Added comments

--- Labs/11_Lab/MonthOutOfRange.java
+++ Labs/11_Lab/MonthOutOfRange.java
@@ -1,9 +1,15 @@
+/*
+ * Throws an error for the month class when a month isn't between 1-12
+ */
+
 public class MonthOutOfRange extends Exception{
     
+    // Default message
     public MonthOutOfRange() {
         super("This isn't a valid month number!");
     }
 
+    // User context message
     public MonthOutOfRange(int monthNumber) {
         super(monthNumber + " isn't a valid month number!");
     }
