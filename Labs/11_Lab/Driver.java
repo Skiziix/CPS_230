d208b0cb04c4deb3a253ad4820016a9bb8b658e1
Cannonball <rearick.seth@gmail.com>
2024-03-06 20:31:33 -0500

    Correctly handles errors and loops as many times as the user wants

--- Labs/11_Lab/Driver.java
+++ Labs/11_Lab/Driver.java
@@ -1,15 +1,69 @@
-public class Driver {
-    public static void main(String[] args) {
-        Month month;
-        try {
-            month = new Month("January");
-            System.out.println("\n\n" + month);
+import javax.swing.JOptionPane;
 
-        } catch (InvalidMonthName e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+public class Driver {
+
+    static Month month;
+    static boolean again;
+    public static void main(String[] args) {
+        
+        do {
+            String userInput = JOptionPane.showInputDialog("Enter a month name or number: ");
 
         
+            try {
+                int monthNumber = Integer.parseInt(userInput);
+                createMonth(monthNumber);
+            } catch (NumberFormatException e){
+                //do string stuff
+                createMonth(userInput);
+            }
+
+        } while (again);
+
+
+        
+    }
+
+    public static void createMonth(String monthName) {
+
+        try {
+            month = new Month(monthName);
+            System.out.println("\n\n" + month);
+                
+        } catch (InvalidMonthName e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Invalid Month Name!"
+                                          ,JOptionPane.ERROR_MESSAGE);
+        }
+
+        askAgain();
+
+    }
+
+    public static void createMonth(int monthNumber) {
+
+            try {
+                month = new Month(monthNumber);
+                System.out.println("\n\n" + month);
+
+                
+            } catch (MonthOutOfRange e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Invalid Month Number!"
+                                            ,JOptionPane.ERROR_MESSAGE);
+            }
+
+            askAgain();
+            
+    }
+
+    public static void askAgain() {
+        
+        Object[] options = {"Yes", "No"};
+
+        int answer = JOptionPane.showOptionDialog(null, "Enter again?: ", "Another Month?: ", JOptionPane.YES_NO_OPTION,
+                                                     JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+                                                    
+        again = (options[answer].equals("Yes")) ? true : false;
+
+    
     }
 }
